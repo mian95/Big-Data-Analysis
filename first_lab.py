15175f8afabcdbc83f5102b706946ae4c9b96d19
mian95 <umairim24@gmail.com>
2025-02-22 19:48:13 +0500

    Uploading the first practical lecture

---
+++ first_lab.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+a = np.array([
+    [
+        [
+            [
+                [
+                    [
+                        [1, 2, 3, 4],
+                        [5, 6, 7, 8]
+                    ],
+                    [
+                        [9, 10, 11, 12],
+                        [13, 14, 15, 16]
+                    ]
+                ],
+                [
+                    [
+                        [17, 18, 19, 20],
+                        [21, 22, 23, 24]
+                    ],
+                    [
+                        [25, 26, 27, 28],
+                        [29, 30, 31, 32]
+                    ]
+                ]
+            ]
+        ]
+    ]
+]
+)
+
+print(a[0,0,0,1,1,1,3])
+print(f"Dimention : {a.ndim}")
+print(f"Shape : {a.shape}")
+print(f"Size: {a.size}")  
+print(f"Data type: {a.dtype}")  
+print(f"Total memoray used are : {a.nbytes}bytes")
